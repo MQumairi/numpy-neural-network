6f0bb353a28df7583c22b66c4c8f372d9974141c
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-12-17 03:41:05 +0000

    converging nn with predict and relu

--- src/util.py
+++ src/util.py
@@ -7,6 +7,20 @@ def one_hot_encode(y):
     return encoded_y
 
 
+def encode_probabilities(y):
+    max_prob = 0
+    max_prob_pos = 0
+
+    for i in range(len(y)):
+        if y[i] > max_prob:
+            max_prob = y[i]
+            max_prob_pos = i
+
+    encoded_y = np.zeros(y.shape)
+    encoded_y[max_prob_pos] = 1
+    return encoded_y
+
+
 def preprocess(X, y, m, n):
     # preprocess X... flatten
     X = X.numpy().reshape(m, n)
@@ -17,3 +31,11 @@ def preprocess(X, y, m, n):
         y_encoded.append(encoding)
     y = y_encoded
     return X, y
+
+
+def decode(encoded_y):
+    value = 0
+    for i in range(len(encoded_y)):
+        if encoded_y[i] == 1:
+            value = i
+    return value
