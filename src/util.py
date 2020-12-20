7d3d68f446e0700ab100a30a36c291d9f14728cc
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-12-20 06:53:14 +0000

    📖 ReadMe ready

--- src/util.py
+++ src/util.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+# One hot encodes an integer into an array of 0s and 1s
+
 
 def one_hot_encode(y):
     encoded_y = np.zeros(10)
     encoded_y[y] = 1
     return encoded_y
+
+# Takes an array of probabilities, replaces the highest probabiliy with 1, and the rest with 0s
 
 
 def encode_probabilities(y):
@@ -19,6 +23,13 @@ def encode_probabilities(y):
     encoded_y = np.zeros(y.shape)
     encoded_y[max_prob_pos] = 1
     return encoded_y
+
+# Takes in the X and y, and prepares them for the NN class used in Task 2. This involves:
+# - flattening the tensor
+# - converting tensores to numpy arrays
+# - transposing the input, so that features are rows, and samples are colums
+# - one-hot encoding all targets
+# Returns two numpy arrays for targets and features
 
 
 def preprocess(X, y, set_features_as_rows=True):
@@ -41,13 +52,16 @@ def preprocess(X, y, set_features_as_rows=True):
         y = y.transpose()
     return X, y
 
+# Takes the features and targe arrays, and slices them using the specied psotions.
+# Useful for reducing the size of the training set for the purposes of testing the network.
+
 
 def shorten(X, y, start, end):
     features = X[:, start:end]
     target = y[:, start:end]
-    # np.array([X[, start:length]])
-    # target = np.array([y[, start:length]])
     return features, target
+
+# Takes in a one-hot encoding array of 0s and 1s, and translates it back to an integer.
 
 
 def decode(encoded_y):
